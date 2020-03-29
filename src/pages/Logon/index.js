905001a9dd7f214a4f63535b80dbb0a9f26f27af
Sandy <shayashi@localhost.localdomain>
2020-03-29 12:24:28 -0300

    Frontend

---
+++ src/pages/Logon/index.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react';
+// Pacote importado pelo 'npm install react-icons'
+// Dentro dele tem vários site de ícones que podem ser utilizados na aplicação.
+// Escolhemos o Feather Icons (FI)
+import { FiLogIn } from 'react-icons/fi'
+import { Link } from 'react-router-dom';
+import api from '../../services/api'
+
+import './styles.css'
+
+import logoImg from '../../assets/logo.svg'
+import hearoesImg from '../../assets/heroes.png'
+
+export default function Logon() {
+
+    const [id, setId] = useState('');
+
+    async function handleLogin(e) {
+
+        e.proventDefault();
+
+        try {
+            const response = await api.post('login', { id });
+
+            console.log(response.data.name)
+        } catch (err) {
+            alert('Falha no login, tente novamente.')
+        }
+    }
+
+    return (
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="Be The Hero" />
+
+                <form onSubmit={handleLogin}>
+                    <h1>Faça seu logon</h1>
+
+                    <input placeholder="Sua ID" 
+                    value={id}
+                    onChange={e => setId(e.target.value)}/>
+                    
+                    <button className="button" type="submit">Entrar</button>
+
+                    <Link className="back-link" to="/register">
+                        < FiLogIn size={16} color="#E02041" />
+                        Não tenho cadastro
+                    </Link>
+                </form>
+            </section>
+            <img src={hearoesImg} alt="Heroes" />
+        </div>
+    );
+}
